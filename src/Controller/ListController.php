44c1b625da3fa0e3da13b0ac989958bdede120cc
htunhtunhtet <ryanhhh91@gmail.com>
2019-08-06 16:54:58 +0800

    image upload update

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -8,6 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\View\View;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -45,6 +49,7 @@ class ListController extends AbstractFOSRestController
 	/**
 	 * @Rest\RequestParam(name="title", description="Title of the list", nullable=false)
 	 * @param ParamFetcher $paramFetcher
+	 * @return View
 	 */
 	public function postListsAction(ParamFetcher $paramFetcher)
 	{
@@ -75,9 +80,56 @@ class ListController extends AbstractFOSRestController
 	
 	}
 	
-	public function toggleListsAction(int $id)
+	/**
+	 * @Rest\FileParam(name="image", description="The background of the list", nullable=false, image=true)
+	 * @param Request $request
+	 * @param ParamFetcher $paramFetcher
+	 * @param $id
+	 * @return View
+	 */
+	public function backgroundListsAction(Request $request, ParamFetcher $paramFetcher, $id)
 	{
+		
+		$list = $this->taskListRepository->findOneBy(['id'=> $id]);
+		$currentBackground  = $list->getBackground();
+		
+		if (!is_null($currentBackground)){
+			$filesSystem    = new Filesystem();
+			$filesSystem    ->remove(
+				$this->getUploadsDir().$currentBackground
+			);
+		}
+		
+		/** @var UploadedFile $file */
+		$file = $paramFetcher->get('image');
+		
+		if ($file){
+			$filename = md5(uniqid()).'.'. $file->guessClientExtension();
+			
+			$file ->move(
+				$this -> getUploadsDir(),
+				$filename
+			);
+			
+			$list->setBackground($filename);
+			$list->setBackgroundPath('/uploads/'.$filename);
+			
+			$this->entityManager ->persist($list);
+			$this->entityManager ->flush();
+			
+			$data = $request->getUriForPath(
+				$list->getBackgroundPath()
+			);
+			
+			return $this->view($data, Response::HTTP_OK);
+		}
+		
+		return $this->view(['message' => 'something went wrong'], Response::HTTP_BAD_REQUEST);
+	}
 	
+	private function getUploadsDir()
+	{
+		return $this->getParameter('uploads_dir');
 	}
     
 }
